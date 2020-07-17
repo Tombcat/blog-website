3bdec0f0a50c82176561cc1dfd083de2651a130b
Tombcat <marcin.wlodarczyk98@gmail.com>
2020-07-17 22:25:44 +0200

    Registration and loggin Api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {EventsComponent} from './events/events.component';
+import {SpecialComponent} from './special/special.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthService} from './auth.service';
+import {EventService} from './event.service';
+import {AuthGuard} from './auth.guard';
+import {TokenInterceptorService} from './token-interceptor.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, EventService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
